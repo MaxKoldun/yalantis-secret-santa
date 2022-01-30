a52127b51b999463d2edc9b658e73e0f52b0377c
Max Koldun <koldun.maks@gmail.com>
2022-01-30 18:30:14 +0200

    Add swagger documentation for the project

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000 || process.env.PORT;
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
 
+app.use('/api-docs', swaggerUi.serve,  swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.json());
 
 require("./routes")(app);
